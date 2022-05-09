14294b932b995f8c3f9640a5b1a66ef0de14c03b
lothbrokragnar <gonzalo.cuevas.verdugo@gmail.com>
2022-05-08 22:50:23 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Crear una nueva tarea</h1>
+
+    <form @submit.prevent="addNewTask()">
+      <label>Tarea </label>
+      <input type="text" placeholder="Ingresa una tarea nueva" v-model="newTasks">
+      <button type="submit">Crear</button>
+    </form>
+    <h2>Lista</h2>
+    <div>
+      <ul v-for="(task, i) in tasks" :key="i">
+        <li>
+          {{ i+1 }}: {{task}}
+          <button @click="deleteTask(i)">Borrar</button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        newTasks: '',
+        tasks: [],
+      };
+    },
+    methods: {
+      addNewTask() {
+        this.tasks.push(this.newTasks)
+        this.newTasks = ''
+      },
+      deleteTask(someTask){
+        this.tasks.splice(someTask, 1)
+      }
+    },
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+*{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
